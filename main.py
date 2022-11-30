b086ea7b134062043be09d5e1e25a015bb6b1cd4
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-11-30 18:15:29 -0500

    working on home api

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
-# This is a sample Python script.
+import pymongo as pymongo
+from flask import Flask, request, jsonify
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+app.config['DEBUG'] = True
 
+client = pymongo.MongoClient("mongodb+srv://MaxDufour:5uwLOgDI1k8Qf1Op@iotfinalproject.ldavcfn.mongodb.net/?retryWrites=true&w=majority")
+db = client.HomeInvasions
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_hi():
+    cursor = db.homes.find()
+    homes = list(cursor)
+    for home in homes:
+        if "_id" in home:
+            home["_id"] = str(home["_id"])
+
+    return jsonify(homes)
+    #return db.homes.insert_one({"name":"Dufour's Home"}).inserted_id
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+app.run()
+
