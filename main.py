adc0630549e029d75740bb1a87294a7d04e48b4d
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-11-30 23:13:19 -0500

    working on home api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pymongo as pymongo
 from flask import Flask, request, jsonify
+from HomeSchema import HomeSchemaPost
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,12 +9,31 @@ client = pymongo.MongoClient("mongodb+srv://MaxDufour:5uwLOgDI1k8Qf1Op@iotfinalp
 db = client.HomeInvasions
 
 
-@app.route('/homes/<home>')
-def print_hi(home):
+@app.route('/homes',methods=["POST"])
+def add_Home():
+    error = HomeSchemaPost().validate(request.json)
+    if error:
+        return error, 400
+
+    db.homes.insert_one(request.json)
+
+    return jsonify({"name" : request.json["name"]})
+@app.route('/homes',methods=["GET"])
+def get_All_Homes():
+    cursor = db.homes.find({},{"name":1,"_id":0})
+    homes = list(cursor)
+
+    return jsonify(homes)
+@app.route('/homes/<home>',methods=["GET"])
+def get_Home_By_Name(home):
     cursor = db.homes.find({"name":home},{"name":1,"_id":0})
     homes = list(cursor)
 
     return jsonify(homes)
+
+
+
+
     #return db.homes.insert_one({"name":"Dufour's Home"}).inserted_id
 
 
