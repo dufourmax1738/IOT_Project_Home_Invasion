20da5ee1f3e23426ba80361f7e780c7ab3529115
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-11-30 19:45:31 -0500

    working on home api

--- HomeSchema.py
+++ HomeSchema.py
@@ -2,4 +2,3 @@ from marshmallow import Schema, fields, validate
 
 class HomeSchemaPost(Schema):
     name = fields.String(required=True, validate=validate.Length(min=3))
-    description = fields.String(required=True, validate=validate.Length(min=5))
