4216f2ded8a9c6172e58ba990e9e5a4a6461d0dc
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-11-30 19:45:34 -0500

    working on home api

--- main.py
+++ main.py
@@ -7,12 +7,11 @@ app.config['DEBUG'] = True
 client = pymongo.MongoClient("mongodb+srv://MaxDufour:5uwLOgDI1k8Qf1Op@iotfinalproject.ldavcfn.mongodb.net/?retryWrites=true&w=majority")
 db = client.HomeInvasions
 
-def print_hi():
-    cursor = db.homes.find()
+
+@app.route('/homes/<home>')
+def print_hi(home):
+    cursor = db.homes.find({"name":home},{"name":1,"_id":0})
     homes = list(cursor)
-    for home in homes:
-        if "_id" in home:
-            home["_id"] = str(home["_id"])
 
     return jsonify(homes)
     #return db.homes.insert_one({"name":"Dufour's Home"}).inserted_id
@@ -21,7 +20,7 @@ def print_hi():
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi()
+    app.run()
 
-app.run()
+
 
