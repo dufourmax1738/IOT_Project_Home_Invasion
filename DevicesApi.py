3b33a876d24fc2d2c848759ec151e28f2221a3f6
dufourmax1738 <77691530+dufourmax1738@users.noreply.github.com>
2022-12-04 12:31:36 -0500

    working on home api

--- DevicesApi.py
+++ DevicesApi.py
@@ -1,7 +1,7 @@
 import pymongo as pymongo
 from pymongo import ReturnDocument
 from flask import Flask, request, jsonify, Blueprint
-import DevicesApi
+from DeviceSchema import DeviceSchema
 
 client = pymongo.MongoClient("mongodb+srv://MaxDufour:5uwLOgDI1k8Qf1Op@iotfinalproject.ldavcfn.mongodb.net/?retryWrites=true&w=majority")
 db = client.HomeInvasions
@@ -9,19 +9,62 @@ db = client.HomeInvasions
 devices = Blueprint('devices', __name__, template_folder='templates')
 
 @devices.route('/homes/<home>/devices',methods=["GET"])
-def get_All_Devices_For_Home():
-    # cursor = db.homes.find({},{"name":1,"_id":0})
-    # homes = list(cursor)
+def get_All_Devices_For_Home(home):
+    cursor = db.homes.aggregate([
+    {
+        '$match': {
+            'name': 'testHome'
+        }
+    }, {
+        '$project': {
+            '_id': 0,
+            'name': 1,
+            'devices': 1
+        }
+    }, {
+        '$project': {
+            'name': 0
+        }
+    }
+])
+    homes = list(cursor)
 
-    return jsonify({"ping":"pong"}), 200
+    return jsonify(homes), 200
 
 @devices.route('/homes/<home>/devices',methods=["POST"])
 def add_Device_For_Home(home):
-    # if error:
-    #     return error, 400
+    error = DeviceSchema().validate(request.json)
+    if error:
+        return error, 400
     query = {"name": home}
     newValue = {"$push": {"devices": request.json}}
-    updatedHome = db.homes.find_one_and_update(query, newValue, {"name": 1, "_id": 0, "devices":1},
+    updatedHome = db.homes.find_one_and_update(query, newValue, {"name": 1, "_id": 0, "devices": 1},
+                                               return_document=ReturnDocument.AFTER, upsert=False, )
+
+    return jsonify(updatedHome), 200
+
+@devices.route('/homes/<home>/devices/<device>',methods=["DELETE"])
+def delete_Device_For_Home(home, device):
+    query = {"name": home}
+    newValue = {"$pull": {"devices": {"name":device}}}
+    updatedHome = db.homes.find_one_and_update(query, newValue, {"name": 1, "_id": 0, "devices": 1},
+                                               return_document=ReturnDocument.AFTER, upsert=False, )
+
+    return jsonify(updatedHome), 200
+
+@devices.route('/homes/<home>/devices/<device>',methods=["PUT"])
+def update_Device_For_Home(home, device):
+
+    error = DeviceSchema().validate(request.json)
+    if error:
+        return error, 400
+    query = {"name": home}
+
+    newValue = {"$pull": {"devices": {"name": device}}}
+    db.homes.find_one_and_update(query, newValue)
+
+    newValue = {"$push": {"devices": request.json}}
+    updatedHome = db.homes.find_one_and_update(query, newValue, {"name": 1, "_id": 0, "devices": 1},
                                                return_document=ReturnDocument.AFTER, upsert=False, )
 
     return jsonify(updatedHome), 200
